b7008d4b3dd73c85c42d679e93e6a24fe731d31e
eminkabacaoglu <eminkabacaoglu@gmail.com>
2022-06-24 21:06:35 +0300

    done

---
+++ README.md
@@ -0,0 +1,39 @@
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=8005016&assignment_repo_type=AssignmentRepo)
+# Ödev 2
+
+Bu haftaki ödevinizde aşağıdaki görevi çözmenizi bekliyoruz.
+
+## Görev
+
+Bugün, **World of Magic** isimli oyunu örnek vererek 2 boyutlu dizileri anlatmıştım.
+Oyunda büyücümüzün 3 adet bildiği büyü vardır. Büyücümüz bu büyüleri kullanarak bir savaşta maximum 9 büyü gerçekleştirebiliyor.
+Göreviniz büyücümüzün minimum sayıda büyü kullanarak bölüm sonu canavarlarını yenip yenemeyeceğini anlayan bir algoritma yazmanız.
+
+Sizin için büyücümüzün savaşı kazanıp kazanmayacağını anlayan kısmı geliştirdik:
+
+```java
+if (minNumberSpellsUsed > maxNumOfAttacksAllowed) {
+	System.out.println("Magician died!");
+} else if (minNumberSpellsUsed > 0 && minNumberSpellsUsed <= maxNumOfAttacksAllowed) {
+	System.out.println("Magician won the battle!");
+} else {
+	System.out.println("Result is not correct!");
+}
+```
+
+Sizin sadece aşağıdaki kod parçasında değişiklik yapmanızı bekliyoruz (`PlayWorldOfMagic.java` dosyasında bu kod parçasını bulabilirsiniz. Comment'e yazılmış uyarıları dikkatli **okuyunuz**.):
+
+```java
+int spellsUsed = 0;
+// ______ BASLANGIC _______ Kodunuz buradan baslamali
+
+// ______ SON _______ Kodunuz burada bitmeli
+/* NOT: ______ BASLANGIC _______ ve ______ SON _______ 
+ * arasina istediginiz kadar sayida satir ekleyebilirsiniz.
+ */
+return spellsUsed;
+```
+
+Onun dışındaki kodlarda yapacağınız değişiklik ödevinizi tamamlamaya **ENGEL** olabilir.
+
+Kolaylıklar diliyorum.
---
+++ src/main/java/com/gokhantamkoc/javabootcamp/odevhafta2/PlayWorldOfMagic.java
@@ -0,0 +1,83 @@
+package com.gokhantamkoc.javabootcamp.odevhafta2;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWorldOfMagic implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWorldOfMagic.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		int maxNumOfAttacksAllowed = 9;
+		String[] magicianSpells = createSpellNameRepository();
+		float[] spellDamageInfo = createSpellDamageRepository();
+		String[] bossNames = createBossNameRepository();
+		float[] bossHps = createBossHPRepository();
+		
+		int minNumberSpellsUsed = resolveBattle(
+				magicianSpells, 
+				spellDamageInfo, 
+				bossNames, 
+				bossHps);
+		
+		if (minNumberSpellsUsed > maxNumOfAttacksAllowed) {
+			System.out.println("Magician died!");
+		} else if (minNumberSpellsUsed > 0 && minNumberSpellsUsed <= maxNumOfAttacksAllowed) {
+			System.out.println("Magician won the battle!");
+		} else {
+			System.out.println("Result is not correct!");
+		}
+	}
+	
+	public static int resolveBattle(
+			String[] magicianSpells,
+			float[] spellDamageInfo,
+			String[] bossNames,
+			float[] bossHPs
+			) {
+		
+		int spellsUsed = 0;
+		// ______ BASLANGIC _______ Kodunuz buradan baslamali
+		// Find the largest number in spell list for maximum damage at one time
+		float maxSpellDamage = 0;
+		for(float damage :spellDamageInfo){
+			if (maxSpellDamage < damage){
+				maxSpellDamage = damage;
+			}
+		}
+		// loop for every boss's health power
+		for (int i=0 ;i<bossHPs.length;i++){
+			float bossPower = bossHPs[i]; // get i. boss's health
+			while (bossPower>0){ // attack i. boss until the power is zero or less then zero
+				spellsUsed++;
+				bossPower -= maxSpellDamage;
+			}
+		}
+		// ______ SON _______ Kodunuz burada bitmeli
+		/* NOT: ______ BASLANGIC _______ ve ______ SON _______ 
+		 * arasina istediginiz kadar sayida satir ekleyebilirsiniz.
+		 */
+		return spellsUsed;
+	}
+	
+	public static String[] createSpellNameRepository() {
+		return new String[]{"Ice Storm", "Chain Lightning", "Magic Missile"};
+	}
+	
+	public static float[] createSpellDamageRepository() {
+		return new float[]{40.0f, 30.0f, 5.0f};
+	}
+	
+	public static String[] createBossNameRepository() {
+		return new String[]{"Dire Rat", "Skeleton Knight", "Undead King"};
+	}
+	
+	public static float[] createBossHPRepository() {
+		return new float[]{15.0f, 45.0f, 60.0f};
+	}
+}
